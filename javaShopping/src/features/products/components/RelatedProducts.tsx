401decf01ed5ede246e428fd50584170135d2dd7
keeprok <140785214+keeprok@users.noreply.github.com>
2025-08-30 00:00:10 +0900

    ✨ : product type 정의

--- javaShopping/src/features/products/components/RelatedProducts.tsx
+++ javaShopping/src/features/products/components/RelatedProducts.tsx
@@ -1,10 +1,78 @@
 import ProductCard from './ProductCard';
 
 export default function RelatedProducts() {
+  const productList = [
+    {
+      id: 1,
+      img: '',
+      brand: '',
+      state: '',
+      message: '',
+      price: '',
+    },
+    {
+      id: 2,
+      img: '',
+      brand: '',
+      state: '',
+      message: '',
+      price: '',
+    },
+    {
+      id: 3,
+      img: '',
+      brand: '',
+      state: '',
+      message: '',
+      price: '',
+    },
+    {
+      id: 4,
+      img: '',
+      brand: '',
+      state: '',
+      message: '',
+      price: '',
+    },
+    {
+      id: 5,
+      img: '',
+      brand: '',
+      state: '',
+      message: '',
+      price: '',
+    },
+    {
+      id: 6,
+      img: '',
+      brand: '',
+      state: '',
+      message: '',
+      price: '',
+    },
+    {
+      id: 7,
+      img: '',
+      brand: '',
+      state: '',
+      message: '',
+      price: '',
+    },
+    {
+      id: 8,
+      img: '',
+      brand: '',
+      state: '',
+      message: '',
+      price: '',
+    },
+  ];
   return (
     <div>
       <div className="flex gap-[14px] ">
-        <ProductCard />
+        {productList.map((i) => (
+          <ProductCard key={i.id} />
+        ))}
         {/*  */}
       </div>
     </div>
---
+++ javaShopping/src/features/products/types/produts.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  id: number;
+  img: string;
+  title: string;
+  state: string;
+  message: string;
+  price: string;
+}
