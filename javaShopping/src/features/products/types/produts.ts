ac70078fc7e9fd78b283fe39e3ca71e9aca0ff2d
keeprok <140785214+keeprok@users.noreply.github.com>
2025-08-30 23:21:45 +0900

    ✨ : type 설정및 배치

--- javaShopping/src/features/products/types/produts.ts
+++ javaShopping/src/features/products/types/produts.ts
@@ -2,7 +2,13 @@ export interface Product {
   id: number;
   img: string;
   title: string;
-  state: string;
+  state: boolean;
   message: string;
   price: string;
 }
+export type ProductCardPT = {
+  brand: string;
+  message: string;
+  price: string;
+  state: boolean;
+};
