2092c0ca046747b636ce41c0875d81dc49184b15
keeprok <140785214+keeprok@users.noreply.github.com>
2025-09-01 23:41:16 +0900

    🎨: product 설정

--- javaShopping/src/features/products/components/ProductCard.tsx
+++ javaShopping/src/features/products/components/ProductCard.tsx
@@ -2,15 +2,14 @@ import type { ProductCardPT } from '../types/produts';
 
 export default function ProductCard({ brand, message, price, state }: ProductCardPT) {
   return (
-    <div className="w-[184px] h-[247px] rounded-[15px] bg-white shadow-sm overflow-hidden">
+    <div className="w-[184px] h-[247px] rounded-[15px] bg-white shadow-sm overflow-hidden border-[#F2F2F2]">
       {/* 이미지 영역 */}
       <div className="w-full h-[120px] bg-gray-100" />
-
       {/* 본문 */}
-      <div className="w-full h-[127px] flex items-center justify-center p-3  border-[#F2F2F2]">
-        <div className="w-full flex flex-col justify-between gap-2">
+      <div className="w-full h-[127px] flex items-center justify-center p-3  ">
+        <div className="w-full flex flex-col justify-between gap-[7px]">
           <div className="text-sm font-semibold">{brand}</div>
-          <div className="text-xs text-gray-500 leading-snug">{message}</div>
+          <div className="text-xs text-[#7C7A7A] leading-snug">{message}</div>
           <div className="text-sm font-bold">{price}</div>
 
           <div className="flex gap-[9px]">
@@ -19,7 +18,7 @@ export default function ProductCard({ brand, message, price, state }: ProductCar
                 state ? 'bg-gray-300 text-black cursor-default' : 'bg-black text-white hover:bg-gray-800'
               }`}
             >
-              {state ? '담김' : '담기'}
+              {state ? '담김!' : '담기'}
             </button>
           </div>
         </div>
@@ -27,3 +26,11 @@ export default function ProductCard({ brand, message, price, state }: ProductCar
     </div>
   );
 }
+/*
+font 스타일 : Montserrat 
+brand : Weight : 500 , size : 16px
+message : Weight : 400 , size : 13px
+price : Weight : 500 , size : 14px
+담기 부분  : with : 43px ,  height : 21px
+text weight : 700size : 10px
+*/
--- javaShopping/src/features/products/components/RelatedProducts.tsx
+++ javaShopping/src/features/products/components/RelatedProducts.tsx
@@ -1,79 +1,17 @@
+import { productList } from '../data/products';
 import ProductCard from './ProductCard';
 
 export default function RelatedProducts() {
-  const productList = [
-    {
-      id: 1,
-      img: '',
-      brand: '',
-      state: '',
-      message: '',
-      price: '',
-    },
-    {
-      id: 2,
-      img: '',
-      brand: '',
-      state: '',
-      message: '',
-      price: '',
-    },
-    {
-      id: 3,
-      img: '',
-      brand: '',
-      state: '',
-      message: '',
-      price: '',
-    },
-    {
-      id: 4,
-      img: '',
-      brand: '',
-      state: '',
-      message: '',
-      price: '',
-    },
-    {
-      id: 5,
-      img: '',
-      brand: '',
-      state: '',
-      message: '',
-      price: '',
-    },
-    {
-      id: 6,
-      img: '',
-      brand: '',
-      state: '',
-      message: '',
-      price: '',
-    },
-    {
-      id: 7,
-      img: '',
-      brand: '',
-      state: '',
-      message: '',
-      price: '',
-    },
-    {
-      id: 8,
-      img: '',
-      brand: '',
-      state: '',
-      message: '',
-      price: '',
-    },
-  ];
   return (
     <div>
+      <div>
+        <div>신발 상품 목록</div>
+        <div>`현재 ${productList.length}개의 상품이 있습니다 `</div>
+      </div>
       <div className="flex gap-[14px] ">
         {productList.map((i) => (
-          <ProductCard key={i.id} />
+          <ProductCard key={i.id} brand={i.brand} message={i.message} price={i.price} state={i.state} />
         ))}
-        {/*  */}
       </div>
     </div>
   );
